a7181f662196745bf788229d3997fcb0cd8fe0bf
GOPAL <131484205+Gopalbhalani137@users.noreply.github.com>
2025-02-05 11:49:10 +0530

    Add/update submission for problem String

---
+++ String.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+            string s;
+        cin>>s;
+        
+        int count = 0;
+        for(auto it : s){
+            if(it == '1')
+                count++;
+        }
+        
+        cout<<count<<endl;   
+    }
+    return 0;
+}
